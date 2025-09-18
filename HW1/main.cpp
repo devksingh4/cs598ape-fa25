75c1d6a3dba5bcc02f71aa43edb705c5ab47e5e3
Dev Singh <dsingh14@illinois.edu>
2025-09-18 15:34:59 -0500

    Use schedule dynamic
    about 0.2s out of 9.7 seconds faster

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -56,7 +56,7 @@ void refresh(Autonoma* c){
    auto focus = c->camera.focus;
 
    int n = 0;
-   #pragma omp parallel for
+   #pragma omp parallel for schedule(dynamic)
    for(n = 0; n<H*W; ++n) 
    { 
       Vector ra = forward+((double)(n%W)/W-.5)*((right))+(.5-(double)(n/W)/H)*((up));
