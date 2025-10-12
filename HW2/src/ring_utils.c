f560d550ac23a2b7bb835717a683e9d9fba58564
ronita2 <ronita2@fa25-cs598a-032.cs.illinois.edu>
2025-10-11 23:36:23 -0500

    remove redundant coeff_mods

--- HW2/src/ring_utils.c
+++ HW2/src/ring_utils.c
@@ -5,13 +5,11 @@ Poly ring_add_mod(Poly x, Poly y, double modulus, Poly poly_mod) {
   Poly sum = poly_add(x, y);
 
   Poly sum_mod = coeff_mod(sum, modulus);
-  sum_mod = coeff_mod(sum_mod, modulus);
 
   Poly quot, rem;
   poly_divmod(sum_mod, poly_mod, &quot, &rem);
 
   Poly rem_mod = coeff_mod(rem, modulus);
-  rem_mod = coeff_mod(rem_mod, modulus);
 
   return rem_mod;
 }
@@ -20,13 +18,11 @@ Poly ring_mul_mod(Poly x, Poly y, double modulus, Poly poly_mod) {
   Poly prod = poly_mul(x, y);
 
   Poly prod_mod = coeff_mod(prod, modulus);
-  prod_mod = coeff_mod(prod_mod, modulus);
 
   Poly quot, rem;
   poly_divmod(prod_mod, poly_mod, &quot, &rem);
 
   Poly rem_mod = coeff_mod(rem, modulus);
-  rem_mod = coeff_mod(rem_mod, modulus);
 
   return rem_mod;
 }
