903408812099d9f3886c3f207aa8194171b96bd2
ronita2 <ronita2@fa25-cs598a-032.cs.illinois.edu>
2025-10-16 02:53:34 -0500

    parallelize poly funcs using openmp

--- HW2/src/poly_utils.c
+++ HW2/src/poly_utils.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 
 Poly create_poly(void) {
   Poly p;
@@ -44,6 +45,8 @@ Poly coeff_mod(Poly p, double modulus) {
   Poly out = create_poly();
   // optim: compute actual degree once and iterate only up to it
   int64_t degree = poly_degree(p);
+  
+  #pragma omp parallel for
   for (int i = 0; i <= degree; i++) {
     double coeff = p.coeffs[i];
     if (fabs(coeff) > 1e-9) {
@@ -60,6 +63,7 @@ Poly poly_add(Poly a, Poly b) {
   int64_t b_deg = poly_degree(b);
   if (b_deg > max_deg) max_deg = b_deg;
   
+  #pragma omp parallel for
   for (int i = 0; i <= max_deg; i++) {
     sum.coeffs[i] = a.coeffs[i] + b.coeffs[i];
   }
@@ -69,6 +73,7 @@ Poly poly_add(Poly a, Poly b) {
 Poly poly_mul_scalar(Poly p, double scalar) {
   Poly res = create_poly();
   int64_t degree = poly_degree(p);
+  #pragma omp parallel for
   for (int i = 0; i <= degree; i++) {
     res.coeffs[i] = p.coeffs[i] * scalar;
   }
@@ -97,20 +102,27 @@ Poly poly_mul(Poly a, Poly b) {
   return res;
 }
 
+
 // optim: reduction for cyclotomic polynomial x^n + 1 (since x^n = -1, we have x^(n+k) = -x^k)
 Poly poly_mod_optimized(Poly p, size_t n) {
   Poly result = create_poly();
-  for (int i = 0; i < MAX_POLY_DEGREE; i++) {
+  
+  // parallelize handling coefficients < n (no race condition)
+  #pragma omp parallel for
+  for (int i = 0; i < n; i++) {
     if (fabs(p.coeffs[i]) > 1e-9) {
-      if (i < n) {
-        result.coeffs[i] = p.coeffs[i];
-      } else {
-        int wrapped_idx = i % n;
-        int num_wraps = i / n;
-        // Apply negation for odd number of wraps
-        double sign = (num_wraps % 2 == 0) ? 1 : -1;
-        result.coeffs[wrapped_idx] += sign * p.coeffs[i];
-      }
+      result.coeffs[i] = p.coeffs[i];
+    }
+  }
+  
+  // sequential handling coefficients >= n (to avoid race conditions)
+  for (int i = n; i < MAX_POLY_DEGREE; i++) {
+    if (fabs(p.coeffs[i]) > 1e-9) {
+      int wrapped_idx = i % n;
+      int num_wraps = i / n;
+      // Apply negation for odd number of wraps
+      double sign = (num_wraps % 2 == 0) ? 1 : -1;
+      result.coeffs[wrapped_idx] += sign * p.coeffs[i];
     }
   }
   
